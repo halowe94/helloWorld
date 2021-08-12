258dcee5de17f48e48031c47b2e9bdfefd788bf3
halowe <haleylowe0@gmail.com>
2021-08-12 14:14:25 -0700

    added variables to js and linked it to html

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+//Var is not block scoped
+
+//var, let, and const are declations
+//the equals sign assigns a value
+var firstName = "Haley";
+let lastName = "Lowe";
+//doesn't change
+const age = 27;
+
+firstName = "You can"
+lastName = "catch these hands"
+
+//type coercion. you add a number to a string it becomes a string
+//concatonation
+console.log('Hello,' + ' ' + firstName + ' ' + lastName + '!' + ' ' + 'You are ' + age + ' years old');
